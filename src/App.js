3b43327b5551eb2ef39cb44a89d388e02daae13c
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-14 09:42:38 -0300

    feat: separates imports and starts game stages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
+// CSS
 import './App.css';
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data: por não haver Default na exportação, importaremos assim:
+import { wordsList } from './data/words';
+
+// Components
 import StartScreen from './componets/StartScreen';
 
+const stages = [
+  { id: 1, name: 'start'},
+  { id: 2, name: 'game'},
+  { id: 3, name: 'end'},
+]
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
   return (
     <div className="App">
       <StartScreen />
