5da9f3cf0258a7ed372f60ca2e76837a76854e63
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-14 13:00:05 -0300

    feat: generate html for Game component

--- src/componets/Game.js
+++ src/componets/Game.js
@@ -2,12 +2,24 @@ import './Game.css'
 
 function Game({verifyLetter}) {
   return (
-    <div>
-      <h1>Game</h1>
-      <button
-        onClick={ verifyLetter }
-      >
-        Finalizar Jogo</button>
+    <div class="game">
+      <p class="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 class="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div class="wordContainer">
+        <span className='letter'>A</span>
+        <span class="blankSquare"></span>
+      </div>
+      <div class="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letter' maxLength="1" required/>
+        </form>
+      </div>
     </div>
   )
 }
