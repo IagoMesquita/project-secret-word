302370b3c7f791f8181da8616075ce59857f2ff1
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-14 16:16:52 -0300

    feat: receives and uses the states coming from props

--- src/componets/Game.js
+++ src/componets/Game.js
@@ -1,30 +1,49 @@
 import './Game.css' 
 
-function Game({verifyLetter}) {
+function Game({
+  verifyLetter, 
+  pickedWord, 
+  pickedCategory, 
+  letters, 
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score
+}) {
+  console.log(pickedWord)
   return (
-    <div class="game">
-      <p class="points">
-        <span>Pontuação: 000</span>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação:{score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
-      <h3 class="tip">
-        Dica sobre a palavra: <span>Dica...</span>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
-      <div class="wordContainer">
-        <span className='letter'>aa</span>
-        <span class="blankSquare"></span>
+      <p>Você ainda têm {guesses} tentativas</p>
+      <div className="wordContainer">
+        {
+          letters.map((letter, index) => (
+            guessedLetters.includes(letter) ? (
+              <span className='letter' key={index}>{letter}</span>
+            ) : (
+              <span className="blankSquare" key={index}>?</span>
+            )
+          ))
+        }
       </div>
-      <div class="letterContainer">
+      <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
         <form>
           <input type="text" name='letter' maxLength="1" required/>
           <button>Jogar!</button>
         </form>
       </div>
-      <div class="wrongLettersContainer">
+      <div className="wrongLettersContainer">
         <p>Letras já utilizadas:</p>
-        <span>a,</span>
-        <span>e,</span>
+        {wrongLetters.map((wrongLetter, i) => (
+          <span key={i}>{wrongLetter}</span>
+        ))}
       </div>
     </div>
   )
