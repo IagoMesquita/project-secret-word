5fcc7fe28b5cf8a67d18dbd604071591d0bab5dd
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-07-14 16:15:52 -0300

    feat: terminates startGame and sends states to Game component

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ function App() {
   const [pickedWord, setPickedWord] = useState("");
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   
   const pickWordAndCategory = () => {
     // pick a random category
@@ -46,6 +52,11 @@ function App() {
     let wordLetters = word.split("");
     wordLetters = wordLetters.map((l) => l.toLowerCase());
 
+    // fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+
     setGameStage(stages[1].name)
   }
 
@@ -62,7 +73,18 @@ function App() {
   return (
     <div className="App">
       { gameStage === 'start' &&  <StartScreen startGame={startGame} /> }
-      { gameStage === 'game' &&  <Game verifyLetter={verifyLetter} /> }
+      { gameStage === 'game' &&  (
+        <Game 
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        /> 
+      )}
       { gameStage === 'end' &&  <GameOver retry={retry} /> }
     </div>
   );
